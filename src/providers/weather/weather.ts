f7674eeed1c55a4d02200c3e1f07cbf8b874e02d
abdulrafay02 <abdulrafay_02@live.com>
2018-09-15 13:19:10 +0500

    adding project to git

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class WeatherProvider {
+
+  API_KEY = 'insert your API key here';
+  url;
+
+  constructor(public http: HttpClient) {
+    this.url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  }
+
+  getWeather(city, country) {
+    return this.http.get(this.url + `${city},${country}&id=524901&APPID=${this.API_KEY}&units=metric`)
+  }
+
+}
