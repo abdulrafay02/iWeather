43ec69fb9b1543603913cc72edecdc168822a78c
abdulrafay02 <abdulrafay_02@live.com>
2018-09-18 10:48:52 +0500

    - removed settings module - fixed blocked mixed content err - static content changes

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,45 +1,43 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Storage } from '@ionic/Storage';
-import { HomePage } from '../home/home'
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { Storage } from "@ionic/Storage";
+import { HomePage } from "../home/home";
 
-@IonicPage()
 @Component({
-  selector: 'page-settings',
-  templateUrl: 'settings.html',
+  selector: "page-settings",
+  templateUrl: "settings.html"
 })
 export class SettingsPage {
-
   city: string;
   country: string;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private storage: Storage) {
-      this.storage.get('location').then((val) => {
-        if(val != null) {
-          let location = JSON.parse(val);
-          this.city = location.city;
-          this.country = localStorage.country;
-        } else {
-          this.city = 'Karachi';
-          this.country = 'PK';
-        }
-      });
+    private storage: Storage
+  ) {
+    this.storage.get("location").then(val => {
+      if (val != null) {
+        let location = JSON.parse(val);
+        this.city = location.city;
+        this.country = localStorage.country;
+      } else {
+        this.city = "Karachi";
+        this.country = "PK";
+      }
+    });
   }
 
   saveForm() {
     let location = {
       city: this.city,
       country: this.country
-    }
-    this.storage.set('location', JSON.stringify(location));
+    };
+    this.storage.set("location", JSON.stringify(location));
     this.navCtrl.push(HomePage);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+    console.log("ionViewDidLoad SettingsPage");
   }
-
 }
--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,18 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class WeatherProvider {
-
-  API_KEY = 'insert your API key here';
+  API_KEY = "insert your API key here";
   url;
 
   constructor(public http: HttpClient) {
-    this.url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    this.url = "https://api.openweathermap.org/data/2.5/weather?q=";
   }
 
   getWeather(city, country) {
-    return this.http.get(this.url + `${city},${country}&id=524901&APPID=${this.API_KEY}&units=metric`)
+    return this.http.get(
+      this.url +
+        `${city},${country}&id=524901&APPID=${this.API_KEY}&units=metric`
+    );
   }
-
 }
